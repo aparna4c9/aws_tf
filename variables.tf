8c49aa32d3255813566b1b2a8a0fc0e539bbbccb
kiran <kiran_baddi@hotmail.com>
2020-01-06 10:27:20 +0530

    added tfvars file

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "profile"  {
 
 variable "ec2_size" {
   description = "This is the instance.type ex: t2.micro, t3.small etc"
-  default = "t2.micro"
+  
 }
 
 variable "key_name" {
   description = "Name of an existing key pair"
-  default = "ranbaddi"
+  
 }
